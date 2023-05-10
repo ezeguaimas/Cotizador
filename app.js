bfb8362f5ec0cd5a6e5d4157649780bb07af0f64
ezeguaimas <ezequiel.guaimas@gmail.com>
2023-05-10 02:17:31 -0300

    cotizador

--- app.js
+++ app.js
@@ -1,18 +1,24 @@
 function calcular() {
-  // Tomamos los valores ingresados en los inputs
-  const aporte = parseFloat(document.getElementById("aporte").value);
-  const anssal = parseFloat(document.getElementById("anssal").value);
-
-  // Realizamos los cálculos
-  const sueldo_bruto = (aporte + anssal) / 0.03;
-  const aporte_os = sueldo_bruto * 0.0765;
-
-  // Mostramos los resultados en el HTML
-  document.getElementById("sueldo_bruto").textContent = sueldo_bruto.toFixed(2);
-  document.getElementById("aporte_os").textContent = aporte_os.toFixed(2);
-}
-
-const borrarBtn = document.getElementById("borrar");
-borrarBtn.addEventListener("click", function () {
-  document.getElementById("anssal", "aporte").value="0"();
-});
+    const aporte = parseFloat(document.getElementById("aporte").value);
+    const anssal = parseFloat(document.getElementById("anssal").value);
+    let sueldoBruto = (aporte + anssal) / 0.03;
+  
+    const montoMaximo = 642142.18;
+  
+    if (sueldoBruto > montoMaximo) {
+      sueldoBruto = montoMaximo;
+    }
+  
+    const aporteOS = sueldoBruto * 0.0765;
+  
+    // Mostramos los resultados en el HTML
+    document.getElementById("sueldo_bruto").textContent = sueldoBruto.toFixed(2);
+    document.getElementById("aporte_os").textContent = aporteOS.toFixed(2);
+  }
+  
+  const borrarBtn = document.getElementById("borrar");
+  borrarBtn.addEventListener("click", function () {
+    document.getElementById("anssal").value = 0;
+    document.getElementById("aporte").value = 0;
+  });
+  
